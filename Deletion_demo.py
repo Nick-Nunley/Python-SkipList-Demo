acf9f94a6dc4fe4b5de3dd4b10a034c0e4cd5852
Nick-Nunley <nnunley@mednet.ucla.edu>
2024-03-06 18:37:54 -0800

    Added a proper singly linked list for comparison

--- Deletion_demo.py
+++ Deletion_demo.py
@@ -1,17 +1,22 @@
 import csv
 import time
+from LinkedList import LinkedList
 from SkipList import SkipList
 
-with open('gene_list.csv', encoding = 'UTF-8') as file:
+gene_list_file = 'gene_list.csv'
+element_to_delete = 'MYCN'
+
+with open(gene_list_file, encoding = 'UTF-8') as file:
     csv_file = csv.reader(file)
     gene_list = [x for row in csv_file for x in row]
 
 gene_list = list(set(gene_list))
 
-gene_skip_list = SkipList()
-gene_skip_list.readCSV('gene_list.csv')
+gene_linked_list = LinkedList()
+gene_linked_list.readCSV(gene_list_file)
 
-element_to_delete = 'MYCN'
+gene_skip_list = SkipList()
+gene_skip_list.readCSV(gene_list_file)
 
 # Testing the deletion speed of regular Python list
 start_time = time.time()
@@ -19,6 +24,13 @@ gene_list.remove(element_to_delete)
 end_time = time.time()
 runtime = end_time - start_time
 print(f'Python list Runtime: {runtime:.8f} seconds')
+
+# Testing the search speed of linked list
+start_time = time.time()
+gene_linked_list.remove_node(element_to_delete)
+end_time = time.time()
+runtime = end_time - start_time
+print(f'Linked list Runtime: {runtime:.8f} seconds')
 
 # Testing the deletion speed of SkipList
 start_time = time.time()
--- Search_demo.py
+++ Search_demo.py
@@ -1,17 +1,22 @@
 import csv
 import time
+from LinkedList import LinkedList
 from SkipList import SkipList
 
-with open('gene_list.csv', encoding = 'UTF-8') as file:
+gene_list_file = 'gene_list.csv'
+element_to_search = 'MYCN'
+
+with open(gene_list_file, encoding = 'UTF-8') as file:
     csv_file = csv.reader(file)
     gene_list = [x for row in csv_file for x in row]
 
 gene_list = list(set(gene_list))
 
-gene_skip_list = SkipList()
-gene_skip_list.readCSV('gene_list.csv')
+gene_linked_list = LinkedList()
+gene_linked_list.readCSV(gene_list_file)
 
-element_to_search = 'MYCN'
+gene_skip_list = SkipList()
+gene_skip_list.readCSV(gene_list_file)
 
 # Testing the search speed of regular Python list
 start_time = time.time()
@@ -19,6 +24,13 @@ print(gene_list[gene_list.index(element_to_search)])
 end_time = time.time()
 runtime = end_time - start_time
 print(f'Python list Runtime: {runtime:.8f} seconds')
+
+# Testing the search speed of linked list
+start_time = time.time()
+print(gene_linked_list.find(element_to_search).data)
+end_time = time.time()
+runtime = end_time - start_time
+print(f'Linked list Runtime: {runtime:.8f} seconds')
 
 # Testing the search speed of SkipList
 start_time = time.time()
